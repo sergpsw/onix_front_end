8266ea901a975ae4d856c1163b59745231155beb
sergpsw <sergpsw@gmail.com>
2019-11-12 12:57:43 +0200

    lesson 2

--- js/main.js
+++ js/main.js
@@ -11,3 +11,32 @@ btn_on.onclick = function() {
 /*--------*/
 
 
+/*Operation on Tasks*/
+let tasksCompleted = document.querySelector(".sidebar-tasks-completed");
+let tasksCompletedCount = document.querySelector(".count-completed");
+let tasksOpenCount = document.querySelector(".count-open");
+
+tasksCompleted.onclick = function tasksCompleted() {
+  if (confirm("Are you sure you want to change the number of tasks?")) {   
+    if (tasksOpenCount.innerHTML > 0) {
+       tasksOpenCount.innerHTML--;
+    }
+    tasksCompletedCount.innerHTML++;
+    if (tasksOpenCount.innerHTML === "0") {
+      alert("No open tasks!");
+  }
+  }
+}
+/*--------*/
+
+
+/*РЎollect images*/
+let imageSelect = document.querySelectorAll(".imgBg");
+let notificationCount = document.querySelector(".count-img");
+
+imageSelect.forEach( function(img, index) {
+  img.addEventListener('click', function() {
+    notificationCount.innerHTML = index;
+ });
+});
+/*--------*/
